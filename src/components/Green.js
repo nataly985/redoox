7dadee4c31b5a866c507b67979e8a3236bf48c5d
Wanda Nataly <uzcateguiw@hotmail.com>
2018-05-15 18:48:18 -0600

    done

--- src/components/Green.js
+++ src/components/Green.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { handleInputValue} from '../ducks/reducer';
 
-export default function Green(){
+function Green(props){
     return(
         <div style={{ backgroundColor: 'green'}}>
           Green
+          <input 
+          type="text"
+          placeholder="Type in here"
+          onChange={(event => props.handleInputValue(event.target.value))}/>
         </div>
     )
 }
+const mapDispatchToProps ={
+handleInputValue
+}
+
+export default connect(null, { mapDispatchToProps})(Green)
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,7 +1,29 @@
-const initialState ={}
+const initialState ={
+    greenInput: ''
+}
 
-// const=
+// action
+const INPUT_VALUE = "INPUT_VALUE";
 
+//anonymous function
+//state= initiatlState is a default input 
 export default function reducer(state = initialState, action){
+switch(action.type){
+    case INPUT_VALUE:
+    console.log(state.greenInput)
+    return Object.assign({}, state, {greenInput: action.payload })
+    
+    //default output
+    default:
+    return state;
+}
+}
 
+//action creators
+export function handleInputValue(value){
+    // console.log('you hit handleInputValue')
+    return { 
+        type: INPUT_VALUE,
+        payload: value
+    }
 }
